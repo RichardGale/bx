52e7c69c979bdf6a288781ab0ff1aacdab63f643
Richard Gale <richard@richardgale.org>
2015-11-20 10:07:32 -0800

    Reverted bad merge.

--- include/bx/os.h
+++ include/bx/os.h
@@ -11,9 +11,7 @@
 
 #if BX_PLATFORM_WINDOWS || BX_PLATFORM_WINRT
 #	include <windows.h>
-#   if !BX_PLATFORM_WINPHONE
-#	    include <psapi.h>
-#   endif
+#	include <psapi.h>
 #elif  BX_PLATFORM_ANDROID \
 	|| BX_PLATFORM_EMSCRIPTEN \
 	|| BX_PLATFORM_FREEBSD \
